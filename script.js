428bf2c91273cbe66bf08b55f631a5b583c110ac
givisab <givisab12345@gmail.com>
2023-09-29 14:32:38 +0400

    Create branch, use classes instead

--- script.js
+++ script.js
@@ -118,11 +118,13 @@ function displayBook(book) {
     });
 }
 
-// Declaring Book object constructor
+// Declaring Book class
 
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
